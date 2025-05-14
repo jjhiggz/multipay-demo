b416773c6ade8eb18e9ec3caead41516d3f87557
Jon Higger <jonathan.higger@gmail.com>
2025-05-14 11:33:06 -0400

    better

--- apps/server/src/db/seed/seed.ts
+++ apps/server/src/db/seed/seed.ts
@@ -17,6 +17,18 @@ const main = async () => {
     password: "password123",
   });
 
+  const canadianUser = await createUser({
+    email: "canada@achairs.com",
+    name: "Canada Chairs",
+    password: "password123",
+  });
+
+  const indiaUser = await createUser({
+    email: "india@achairs.com",
+    name: "India Chairs",
+    password: "password123",
+  });
+
   const americanChairs = await createOrganization({
     name: "AmericanChairs",
     slug: "american-chairs",
@@ -27,17 +39,23 @@ const main = async () => {
     organizationId: americanChairs.id,
     role: "owner",
   });
+  await addUserToOrganization({
+    userId: canadianUser.user.id,
+    organizationId: americanChairs.id,
+    role: "member",
+  });
+  await addUserToOrganization({
+    userId: indiaUser.user.id,
+    organizationId: americanChairs.id,
+    role: "member",
+  });
 
   // Create a profile for jon@achairs.com
-  await createProfile(
-    { email: jonAmerica.user.email },
-    {
-      sameCurrencySupported: true,
-      expectedTradeCurrency: "USD",
-      expectedPayoutCurrency: "USD",
-      regionalAccountingCurrency: "USD",
-    }
-  );
+  await createProfile({ email: jonAmerica.user.email }, {});
+  // Create a profile for canada@achairs.com
+  await createProfile({ email: canadianUser.user.email }, { region: "CA" });
+  // Create a profile for india@achairs.com
+  await createProfile({ email: indiaUser.user.email }, { region: "IN" });
 
   await seedHelpers.createRecipientsForOrganization(
     americanChairs.id,
